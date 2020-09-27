94372d676c46fcd9bc66bd33d7fb3ae811e31e0e
Raunaq Bahl <r.bahl@uqconnect.edu.au>
2020-09-27 16:56:50 +1000

    fixed css, added dynamic card adding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
+import Card from './Card';
 import SearchBox from './SearchBox';
 import { users } from './users';
 
@@ -8,7 +9,7 @@ export class App extends Component {
   constructor() {
     super()
     this.state = {
-      users: users,
+      users: [],
       search_field: ''
     }
   }
@@ -21,16 +22,36 @@ export class App extends Component {
     this.setState({search_field: event.target.value})
   }
 
+
+   handleClick = () => {
+    this.setState({
+      users: [this.state.users, 
+              <Card
+              key = {0} 
+              id = {users[0].id} 
+              name  = {users[0].name}
+              date = {users[0].date}
+              title = {users[0].title}
+              description = {users[0].description}
+              />]
+    })
+  }
+
   render() {
+    {/*
     const filteredUsers = this.state.users.filter( users => {
       return users.name.toLowerCase().includes(this.state.search_field.toLowerCase());
-    })
+    }) */}
 
     return (
       <div className = "tc">
-        <h1> Men's Shed App </h1>
-        <SearchBox searchChange = {this.onSearchChange} />
-        <CardList users = {filteredUsers} />
+        <div className = "main-display">
+          <h1 className = "f-subheadline lh-title"> Men's Shed App </h1>                                                                      
+          <SearchBox searchChange = {this.onSearchChange} />
+          <a className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-black" href =  "#" onClick = {this.handleClick} >Add Story</a>
+        </div>   
+         {this.state.users}     
+        {/* <CardList users = {filteredUsers} /> */}
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
 	const {name, date, title, description} = props;
 	console.log(date);
 	return (
-		<div className = 'card tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<div className = 'tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<div className = "flex justify-between">
 				<h4> {name} </h4>
 				<div className = "item">
@@ -15,19 +15,23 @@ const Card = (props) => {
 			<div className = "recording-container">
 			</div>
 			<div className = "scrollable-imgs">
+			
 				<div className = "scrolling-wrapper">
 					<div className = "photo"> <img src = {require("./img_avatar.png")} alt = "profile_img"/> </div>
 					<div className = "photo"> <img src = {require("./img_avatar.png")} alt = "profile_img"/> </div>
 					<div className = "photo"> <img src = {require("./img_avatar.png")} alt = "profile_img"/> </div>
 					<div className = "photo"> <img src = {require("./img_avatar.png")} alt = "profile_img"/> </div>
 					<div className = "photo"> <img src = {require("./img_avatar.png")} alt = "profile_img"/> </div>
-				</div>
+				</div> 
+			
 			</div>
 			<div className = "description-text">
-				<div className = "flex space-between">
-					<h3> {title} </h3>
+				<div className = "flex justify-between">
+					<div className = "flex flex-column justify-center">
+						<h3> {title} </h3>
+					</div>
 					<div className = "card-desc">
-						<p> {description} </p>
+						<p className = "tl f6 lh-copy measure-narrow"> {description} </p>
 					</div>
 				</div>
 			</div>
